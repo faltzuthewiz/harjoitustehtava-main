74cf0bb1dc212370e7e639cb229db89b205b630e
Sonja Fallstrom <jawnlemon@gmail.com>
2025-09-26 13:17:53 +0300

    step 2: read tax_rules.json

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import { loadCSV } from "./loadCSv.js";
+import fs from "fs";
 
 console.log("Node.js + SQLite");
 
 
 // Step 1: load CSV files and update them to database
+
 loadCSV("customers.csv", "customers", ["customer_id", "customer_name"]);
 loadCSV("products.csv", "products", ["sku", "name", "unit_price", "vat_code", "reorder_point"]);
 loadCSV("stock_levels.csv", "stock_levels", ["sku", "warehouse", "qty_on_hand"]);
 loadCSV("orders.csv", "orders", ["order_id", "customer_id", "order_date"]);
 loadCSV("order_lines.csv", "order_lines", ["order_id", "sku", "qty"]);
+
+
+
+// Step 2: read tax-rules.json
+const data = fs.readFileSync("tax_rules.json");
+const taxData = JSON.parse(data);
+
+/* Checking tax-rules
+console.log(taxData.vat.STANDARD);
+console.log(taxData.vat.REDUCED);
+console.log(taxData.vat.ZERO);
+*/
+
